5814cb5b75d5457e9cae847243b21470131cee13
Liam Rossi <liam.rossi199@gmail.com>
2022-01-19 10:37:11 +0100

    upd

--- src/main/java/it/kekw/clowngg/riot/RiotMgrInterface.java
+++ src/main/java/it/kekw/clowngg/riot/RiotMgrInterface.java
@@ -1,14 +1,9 @@
 package it.kekw.clowngg.riot;
 
-import java.io.IOException;
-
-import org.apache.http.client.ClientProtocolException;
-
 import it.kekw.clowngg.riot.dto.SummonerDTO;
 
 public interface RiotMgrInterface {
 
-    public SummonerDTO getSummonerInfoBySummonerName(String summonerName)
-            throws ClientProtocolException, IOException, Exception;
+    public SummonerDTO getSummonerInfoBySummonerName(String summonerName);
 
 }

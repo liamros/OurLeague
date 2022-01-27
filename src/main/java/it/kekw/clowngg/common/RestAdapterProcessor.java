a104786e677d7c281675803bcef776818ece774d
Liam Rossi <liam.rossi199@gmail.com>
2022-01-27 12:02:33 +0100

    restProcessor upd for spring proxies

--- src/main/java/it/kekw/clowngg/common/RestAdapterProcessor.java
+++ src/main/java/it/kekw/clowngg/common/RestAdapterProcessor.java
@@ -6,12 +6,14 @@ import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.aop.framework.Advised;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ClassUtils;
 
 @Component
 public class RestAdapterProcessor implements BeanPostProcessor, ApplicationContextAware {
@@ -23,17 +25,16 @@ public class RestAdapterProcessor implements BeanPostProcessor, ApplicationConte
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RestAdapterProcessor.class);
 
-
-    
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         return bean;
     }
 
-    
-    /** 
-     * if the bean is a RestAdapter type this method instantiates a proxy on the interface intended for it.
-     * Further it fetches from the spring cointainer the bean to which is mapped, and proceeds to populate the
+    /**
+     * if the bean is a RestAdapter type this method instantiates a proxy on the
+     * interface intended for it.
+     * Further it fetches from the spring cointainer the bean to which is mapped,
+     * and proceeds to populate the
      * field of the interface type with the new proxy
      */
     @Override
@@ -41,7 +42,6 @@ public class RestAdapterProcessor implements BeanPostProcessor, ApplicationConte
         Class<?> clazz = aBean.getClass();
         if (!clazz.equals(RestAdapter.class))
             return aBean;
-
 
         String s = restAdapterUsers.get(aBeanName);
         if (s == null)
@@ -53,6 +53,18 @@ public class RestAdapterProcessor implements BeanPostProcessor, ApplicationConte
                 new Class[] { ra.getInterfaceProxy() }, ra);
 
         Object o = applicationContext.getBean(s);
+
+        
+        /* checks if the bean is a spring proxy, in that case, the user class will be
+         * different by the object's which will be a CGLIB-generated subclass */
+        if (!ClassUtils.getUserClass(o).equals(o)) {
+            try {
+                o = ((Advised) o).getTargetSource().getTarget();
+            } catch (Exception e) {
+                LOGGER.error("ERROR: Error while getting target from CGLIB-generated subclass", e);
+                throw new RuntimeException();
+            }
+        }
 
         for (Field field : o.getClass().getDeclaredFields()) {
             field.setAccessible(true);

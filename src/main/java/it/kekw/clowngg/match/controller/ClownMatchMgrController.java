6c5c37d115327d3c5bae266a2250d49b7251ac78
Liam Rossi <liam.rossi199@gmail.com>
2022-02-06 16:34:39 +0100

    dto corrected

--- src/main/java/it/kekw/clowngg/match/controller/ClownMatchMgrController.java
+++ src/main/java/it/kekw/clowngg/match/controller/ClownMatchMgrController.java
@@ -3,6 +3,7 @@ package it.kekw.clowngg.match.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import it.kekw.clowngg.match.impl.ClownMatchMgrImpl;
 import it.kekw.clowngg.riot.dto.SummonerDTO;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("match")
 public class ClownMatchMgrController implements ClownMatchMgr {
 

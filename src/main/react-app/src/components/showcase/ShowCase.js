4c1568c716f1df759dfd47ebe4504909aed133f8
Liam Rossi <liam.rossi199@gmail.com>
2022-02-09 19:57:31 +0100

    showcase

--- src/main/react-app/src/components/showcase/ShowCase.js
+++ src/main/react-app/src/components/showcase/ShowCase.js
@@ -1,50 +1,73 @@
+import { Container } from "@mui/material";
+import { motion } from "framer-motion";
 import * as React from 'react';
 import ShowCaseDetail from "./ShowCaseDetail";
-import { motion } from "framer-motion"
 
-class ShowCase extends React.Component {
+const ShowCase = () => {
 
-    constructor(props) {
-        super(props)
-        this.state = {showCaseDetails: null}
-    }
 
-    componentDidMount() {
-        this.getShowCaseDetails()
-    }
 
-    getShowCaseDetails() {
+    const [showCaseDetails, setShowCaseDetails] = React.useState(null)
+
+    React.useEffect(() => {
+        getShowCaseDetails()
+    }, [])
+
+    const getShowCaseDetails = () => {
         fetch('http://localhost:8080/match/getShowCaseDetails', { mode: 'cors' })
             .then(res => res.json())
             .then((data) => {
-                this.setState({showCaseDetails: data})
+                setShowCaseDetails(data)
             }).catch(console.log)
     }
 
+    // let list = [4070, 5213, 1641]
+    // var list = [{ statName: "KDA", summonerName: "Shakobi", value: 2.7, description: "kekw" }, { statName: "WinRate", summonerName: "Cavendish31", value: 55, description: "kekw" }]
+    
+    const variants = {
+        visible: i => ({
+          opacity: 1,
+          transition: {
+            delay: i * 0.5,
+          },
+        }),
+        hidden: { opacity: 0 },
+      }
 
-
-    render() {
-
-        // let list = [4070, 5213, 1641]
-        // var list = [{ statName: "KDA", summonerName: "Shakobi", value: 2.7, description: "kekw" }, { statName: "WinRate", summonerName: "Cavendish31", value: 55, description: "kekw" }]
-        return (
-            //<Container style={{width: "100%", height: "100%", margin: "0%", marginTop : "2%", padding: "0%", display: "inline-block"}}>
-            <motion.div animate={{ scale: 0.8 }} style={{width: "100%", height: "100%", margin: "0%", marginTop : "2%", padding: "0%", display: "inline-block"}}>
-                {
-                this.state.showCaseDetails ?
-                this.state.showCaseDetails.map(element => {
-                    return <ShowCaseDetail key={element.statName} stats={element} /*profileIconNum={list[index]}*/ />
-                }) : <p>'Loading...'</p>
+    return (
+        <Container style={{width: "100%", height: "100%", margin: "0%", padding: "0%", display: "inline-block"}}>
+            {
+                showCaseDetails ?
+                    showCaseDetails.map((element, i) => {
+                        return (
+                            <motion.li
+                                custom={i}
+                                style={styles.detailContainer}
+                                initial="hidden"
+                                animate="visible"
+                                variants={variants}
+                            >
+                                <ShowCaseDetail key={element.statName} stats={element} /*profileIconNum={list[index]}*/ />
+                            </motion.li>
+                        )
+                    }) : <p>'Loading...'</p>
             }
-            </motion.div>
-            //</Container>
+        </Container>
 
-        )
+    )
 
 
-    }
+
 }
 
+const styles = {
+    detailContainer: {
+        display: "inline-block",
+        width: '29%',
+        margin: "2%"
+    }
+
+}
 
 
 export default ShowCase
--- src/main/react-app/src/components/showcase/ShowCaseDetail.js
+++ src/main/react-app/src/components/showcase/ShowCaseDetail.js
@@ -1,86 +1,100 @@
-import { Card, CardMedia, Typography } from "@mui/material";
+import { Card, CardMedia, CircularProgress, Typography } from "@mui/material";
+import { motion } from "framer-motion";
 import * as React from 'react';
 
 
 
-class ShowCaseDetail extends React.Component {
+const ShowCaseDetail = (props) => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            stats: props.stats,
-            profileIconNum: props.stats.profileIconNum,
-            profileIcon: null
-        };
-    }
 
-    componentDidMount() {
-        this.getSummonerIcon()
-    }
+    const [stats, setStats] = React.useState(props.stats)
+    const [profileIcon, setProfileIcon] = React.useState(null)
 
-    getSummonerIcon() {
-        let url = `http://localhost:8080/match/getProfileIcon?profileIconNumber=${this.state.profileIconNum}`
+    React.useEffect(() => {
+        getSummonerIcon()
+    }, [])
+
+    const getSummonerIcon = () => {
+        let url = `http://localhost:8080/match/getProfileIcon?profileIconNumber=${stats.profileIconNum}`
         fetch(url, { mode: 'cors' })
             .then(response =>
                 response.blob())
             .then(imgBlob => {
                 const img = URL.createObjectURL(imgBlob)
-                this.setState({ profileIcon: img })
+                setProfileIcon(img)
             })
             .catch(error =>
                 console.log(error))
     }
 
-    render() {
-        var statName = this.state.stats.statName
-        var description = this.state.stats.description
-        var summonerName = this.state.stats.summonerName
-        var value = this.state.stats.value
-        return (
-            <Card 
-            elevation={10} 
-            style={styles.card}
-            className="showcaseDetail"              
-            >
-                {
-                    this.state.profileIcon ?
-                    (<CardMedia
-                        component="img"
-                        image={this.state.profileIcon}
-                        style={styles.cardMedia}
-                    />) : null
-                }
-                <Typography style={styles.typography}>{summonerName}</Typography>
-                <Typography style={styles.typography}>{statName}</Typography>
-                {value ? 
-                <Typography style={styles.typography}>{value}</Typography> 
-                : <Typography style={styles.typography}>{description}</Typography>}
-                
-                
-            </Card>
-        );
-    }
+
+    var statName = stats.statName
+    var description = stats.description
+    var summonerName = stats.summonerName
+    var value = stats.value
+    return (
+        <motion.div 
+            // drag="x"
+            // dragConstraints={{ left: -100, right: 100 }}
+            whileHover={{ scale: 1.1 }}
+            // whileTap={{ scale: 0.9 }}
+            style={styles.container}>
+            {
+                profileIcon ?
+                    (<Card
+                        elevation={10}
+                        style={styles.card}
+                        className="showcaseDetail"
+                    >
+                        <CardMedia
+                            component="img"
+                            image={profileIcon}
+                            style={styles.cardMedia}
+                        />
+                        <Typography style={styles.typographyTitle}>{summonerName}</Typography>
+                        <Typography style={styles.typography}>{statName}</Typography>
+                        {value ?
+                            <Typography style={styles.typography}>{value}</Typography>
+                            : <Typography style={styles.typography}>{description}</Typography>}
+
+
+                    </Card>) : <CircularProgress />}
+        </motion.div>
+    );
+
 }
 
 const styles = {
     typography: {
         margin: "2%",
         color: "rgb(208, 168, 92)",
-        fontWeight: "bold"
+    },
+    typographyTitle: {
+        margin: "2%",
+        color: "rgb(208, 168, 92)",
+        fontWeight: "bold",
+        fontSize: "1.2rem"
     },
     card: {
-        width: '29%',
+        width: '100%',
         display: "inline-block",
-        margin: "2%",
-        backgroundColor: "rgb(6, 28, 37)"
+        margin: "0%",
+        backgroundColor: "rgb(6, 28, 37)",
+        border: "3px solid rgb(208, 168, 92)"
     },
     cardMedia: {
-        borderRadius: "50%", 
-        width: "20%", 
-        margin: "auto", 
-        marginTop: "5%", 
+        borderRadius: "50%",
+        width: "20%",
+        margin: "auto",
+        marginTop: "5%",
         border: "2px solid rgb(208, 168, 92)"
-    } 
+    },
+    container: {
+        width: '100%',
+        display: "inline-block",
+        margin: "0%",
+        padding: "0%"
+    }
 }
 
 export default ShowCaseDetail

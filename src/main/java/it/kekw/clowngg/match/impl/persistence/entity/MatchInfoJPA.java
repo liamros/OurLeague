8807a070f306f489061c6c30f9c11186efd9ce33
Liam Rossi <liam.rossi199@gmail.com>
2022-02-17 12:09:38 +0100

    MatchInfo

--- src/main/java/it/kekw/clowngg/match/impl/persistence/entity/MatchInfoJPA.java
+++ src/main/java/it/kekw/clowngg/match/impl/persistence/entity/MatchInfoJPA.java
@@ -1,63 +1,42 @@
 package it.kekw.clowngg.match.impl.persistence.entity;
 
-import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import lombok.Data;
 
 @Entity
 @Table(name = "MATCH_INFO")
-@IdClass(MatchInfoJPA.PrimaryKeys.class)
 public class MatchInfoJPA {
 
-    @Data
-    @SuppressWarnings("unused")
-    public static class PrimaryKeys implements Serializable {
-        private String matchId;
-        private Integer summInfoId;
-        public PrimaryKeys() {
-        }   
-        public PrimaryKeys(String matchId, Integer summInfoId){
-            this.matchId = matchId;
-            this.summInfoId = summInfoId;
-        }   
-    }
 
     
     @Id
     @Column(name = "MATCH_ID")
     private String matchId;
-    @Id
-    @Column(name = "SUMM_INFO_ID")
-    private Integer summInfoId;
     @Column(name = "QUEUE_TYPE_ID")
     private Integer queueTypeId;
     @Column(name = "PATCH")
     private String patch;
     @Column(name = "GAME_CREATION_TIME")
     private Timestamp gameCreationTime;
-    @ManyToOne
-    @JoinColumn(name="SUMM_INFO_ID", referencedColumnName = "ID", insertable = false, updatable = false)
-    private SummonerInfoJPA summoner;
+    @Column(name = "GAME_START_TIME")
+    private Timestamp gameStartTime;
+    @Column(name = "GAME_END_TIME")
+    private Timestamp gameEndTime;
+    @OneToMany
+    @JoinColumn(name="MATCH_ID", referencedColumnName = "MATCH_ID", insertable = false, updatable = false)
+    private List<RelSummonerMatchJPA> relSummonerMatches;
     public String getMatchId() {
         return matchId;
     }
     public void setMatchId(String matchId) {
         this.matchId = matchId;
-    }
-    public Integer getSummInfoId() {
-        return summInfoId;
-    }
-    public void setSummInfoId(Integer summInfoId) {
-        this.summInfoId = summInfoId;
     }
     public Integer getQueueTypeId() {
         return queueTypeId;
@@ -77,16 +56,25 @@ public class MatchInfoJPA {
     public void setGameCreationTime(Timestamp gameCreationTime) {
         this.gameCreationTime = gameCreationTime;
     }
-    public SummonerInfoJPA getSummoner() {
-        return summoner;
+    public Timestamp getGameStartTime() {
+        return gameStartTime;
     }
-    public void setSummoner(SummonerInfoJPA summoner) {
-        this.summoner = summoner;
+    public void setGameStartTime(Timestamp gameStartTime) {
+        this.gameStartTime = gameStartTime;
+    }
+    public Timestamp getGameEndTime() {
+        return gameEndTime;
+    }
+    public void setGameEndTime(Timestamp gameEndTime) {
+        this.gameEndTime = gameEndTime;
+    }
+    public List<RelSummonerMatchJPA> getRelSummonerMatches() {
+        return relSummonerMatches;
     }
     @Override
     public String toString() {
-        return "MatchInfoJPA [gameCreationTime=" + gameCreationTime + ", matchId=" + matchId + ", patch=" + patch
-                + ", queueTypeId=" + queueTypeId + ", summInfoId=" + summInfoId + "]";
+        return "MatchInfoJPA [gameCreationTime=" + gameCreationTime + ", gameEndTime=" + gameEndTime
+                + ", gameStartTime=" + gameStartTime + ", matchId=" + matchId + ", patch=" + patch + ", queueTypeId="
+                + queueTypeId + "]";
     }
-
 }

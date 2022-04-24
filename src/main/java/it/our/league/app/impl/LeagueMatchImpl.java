26be4140b13ca96766bc9b5236602256a48b6ca9
Liam Rossi <liam.rossi199@gmail.com>
2022-04-24 20:54:29 +0200

    upd

--- src/main/java/it/our/league/app/impl/LeagueMatchImpl.java
+++ src/main/java/it/our/league/app/impl/LeagueMatchImpl.java
@@ -8,7 +8,6 @@ import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.Transactional;

56cf8dd92e1ea866871409400ea48cc6b2c51c5c
Liam Rossi <liam.rossi199@gmail.com>
2022-05-05 12:23:29 +0200

    websocket

--- src/main/react-app/src/components/header/UpdateButton.js
+++ src/main/react-app/src/components/header/UpdateButton.js
@@ -1,33 +1,30 @@
+import { CircularProgress } from "@mui/material";
 import Button from '@mui/material/Button';
 import React from "react";
+import { connect } from "react-redux";
 import * as SockJS from 'sockjs-client';
 import * as Stomp from 'stompjs';
+import { fetchShowCaseRankings, fetchHomeLineCharts } from "../../actions";
 
 
-const UpdateButton = () => {
+const UpdateButton = ({fetchShowCaseRankings ,fetchHomeLineCharts}) => {
 
-    const onClick = () => {
-        stompClient.send(
-            "/requestRefreshData",
-            {},
-            "kekw"
-          );
-    }
+    const [updating, setUpdating] = React.useState(false)
 
     var stompClient
-    React.useEffect(() => {
+    const onClick = () => {
+        setUpdating(true)
         var socket = new SockJS('http://localhost:5000/ws');
         stompClient = Stomp.over(socket)
         stompClient.connect({}, onConnected)
-    }, [])
+    }
+
     const onConnected = () => {
         console.log("onConnected");
         stompClient.subscribe("/topic/dataRefresh", onMessageReceived);
-    
         stompClient.send(
-          "/requestRefreshData",
-          {},
-          "kekw"
+            "/requestRefreshData",
+            {}
         );
     }
 
@@ -35,15 +32,22 @@ const UpdateButton = () => {
         console.log("onMessageReceived");
         var message = JSON.parse(payload.body);
         console.log(message)
+        stompClient.disconnect()
+        setUpdating(false)
+        
+        fetchShowCaseRankings()
+        fetchHomeLineCharts()
     }
     return (
         <>
-            <Button
-                sx={{ my: 2, color: 'white', display: 'block' }}
-                onClick={onClick}
-            >
-                Update
-            </Button>
+            {updating ? <CircularProgress /> :
+                <Button
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                    onClick={onClick}
+
+                >
+                    Update
+                </Button>}
         </>
 
     )
@@ -51,4 +55,14 @@ const UpdateButton = () => {
 
 }
 
-export default UpdateButton
+function mapDispatchToProps(dispatch) {
+    return {
+        fetchShowCaseRankings: () => dispatch(fetchShowCaseRankings()),
+        fetchHomeLineCharts: () => dispatch(fetchHomeLineCharts())
+    }
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(UpdateButton)
